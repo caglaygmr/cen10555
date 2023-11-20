d6d801779da8c369f541ac08709aef0c5375ca2d
Çağla <117013770+caglaygmr@users.noreply.github.com>
2023-11-20 18:20:02 +0300

    küp

---
+++ küp.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+double ort(int a, int b, int c) {
+    double x;
+	
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    x = (a + b + c) / 3.0;
+    printf("%lf\n", x);
+    
+    return x;
+}
+
