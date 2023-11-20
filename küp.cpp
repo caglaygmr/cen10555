82313a2240a52198fd18b136c565856929e0aa0b
bengumec <151138278+bengumec@users.noreply.github.com>
2023-11-20 18:30:46 +0300

    Update küp.cpp

--- küp.cpp
+++ küp.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-double ort(int a, int b, int c) {
+double ort(int a, int b); {
     double x;
 	
-    printf("Enter three numbers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    x = (a + b + c) / 3.0;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    x = (a + b) / 2.0;
     printf("%lf\n", x);
     
     return 0;
